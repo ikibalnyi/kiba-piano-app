d95034b6d624bb5b97897d39ebe6110ba9e40cb4
Illia Kibalnyi <illia.kibalnyi@dunnhumby.com>
2019-07-28 15:18:42 +0200

    Add recording functionality

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import Piano from '../Piano';
 
-function App() {
+const App = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordedNotes, setRecordedNotes] = useState([]);
+  const [startRecordingTime, setStartRecordingTime] = useState(null);
+
+  const toggleRecording = () => {
+    setIsRecording(!isRecording);
+
+    if (isRecording) {
+      setStartRecordingTime(null);
+      setRecordedNotes([...recordedNotes].sort((a, b) => a.startTime > b.startTime ? 1 :
+        a.startTime < b.startTime ? -1 : 0));
+    } else {
+      setRecordedNotes([]);
+    }
+  };
+
+  const handlePlayNote = () => {
+    if (isRecording && !startRecordingTime) {
+      setStartRecordingTime(Date.now());
+    }
+  };
+
+  const handleRecordNote = ({ midiNumber, startTime, duration }) => {
+    if (isRecording) {
+      setRecordedNotes([
+        ...recordedNotes,
+        {
+          midiNumber,
+          startTime: startTime - startRecordingTime,
+          duration,
+        },
+      ]);
+    }
+  };
+
   return (
     <div className="App">
       <h1>React Piano Task</h1>
-      <Piano />
+      <Piano
+        isRecording={isRecording}
+        onPlayNote={handlePlayNote}
+        onRecordNote={handleRecordNote}
+      />
+      <div>
+        <button onClick={toggleRecording}>{!isRecording ? 'Start' : 'Stop'} recording</button>
+      </div>
+      <div>
+        <strong>Recorded notes</strong>
+        <div>{JSON.stringify(recordedNotes)}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Piano.js
+++ src/Piano.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Piano as ReactPiano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
-import SoundfontProvider from './SoundfontProvider';
 import 'react-piano/dist/styles.css';
+
+import SoundfontProvider from './SoundfontProvider';
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
@@ -16,7 +18,32 @@ const keyboardShortcuts = KeyboardShortcuts.create({
   keyboardConfig: KeyboardShortcuts.HOME_ROW,
 });
 
-function Piano() {
+const Piano = ({ isRecording, onPlayNote, onRecordNote }) => {
+  const [playingNotes, setPlayingNotes] = useState({});
+
+  const handlePlayNoteInput = (midiNumber, event) => {
+    if (isRecording && !playingNotes[midiNumber]) {
+      setPlayingNotes({
+        ...playingNotes,
+        [midiNumber]: Date.now(),
+      });
+    }
+
+    onPlayNote && onPlayNote(midiNumber, event);
+  };
+
+  const handleStopNoteInput = (midiNumber) => {
+    if (isRecording) {
+      const { [midiNumber]: startTime, ...restPlayingNotes } = playingNotes;
+      if (startTime) {
+        const duration = Date.now() - startTime;
+
+        setPlayingNotes(restPlayingNotes);
+        onRecordNote && onRecordNote({ midiNumber, startTime, duration });
+      }
+    }
+  };
+
   return (
     <SoundfontProvider
       instrumentName="acoustic_grand_piano"
@@ -24,18 +51,32 @@ function Piano() {
       hostname={soundfontHostname}
       render={({ isLoading, playNote, stopNote }) => (
         <div>
-          <ReactPiano
-            disabled={isLoading}
-            noteRange={noteRange}
-            playNote={playNote}
-            stopNote={stopNote}
-            width={1000}
-            keyboardShortcuts={keyboardShortcuts}
-          />
+          <div>
+            <ReactPiano
+              disabled={isLoading}
+              noteRange={noteRange}
+              playNote={playNote}
+              stopNote={stopNote}
+              onPlayNoteInput={handlePlayNoteInput}
+              onStopNoteInput={handleStopNoteInput}
+              width={1000}
+              keyboardShortcuts={keyboardShortcuts}
+            />
+          </div>
+          <div>
+            <strong>Playing notes</strong>
+            <div>{JSON.stringify(playingNotes)}</div>
+          </div>
         </div>
       )}
     />
   );
-}
+};
+
+Piano.propTypes = {
+  isRecording: PropTypes.bool,
+  onPlayNote: PropTypes.func,
+  onRecordNote: PropTypes.func,
+};
 
 export default Piano;
